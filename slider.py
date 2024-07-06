bd4eba9c71a9faf05f4638877d0123cd853201bf
PythonManRemastered <109542449+PythonManRemastered@users.noreply.github.com>
2024-07-06 16:18:58 +0530

    Update slider.py

--- slider.py
+++ slider.py
@@ -33,8 +33,8 @@ project_intro = """
 header = st.container()
 header.title(f"How much have we spent: {money_spent}")
 header.write("""
-<div style="position: absolute; top: 1000; right: 10290;">
-    <div class='fixed-header'></div>
+<div style="position: fixed; top: 0; right: 0; background-color: #f0f0f0; padding: 10px;">
+    Your fixed header content here
 </div>
 """, unsafe_allow_html=True)
 
