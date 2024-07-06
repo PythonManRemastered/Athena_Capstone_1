7d8665f28865eeeb0a549e5af7b491d5e56c26f3
PythonManRemastered <109542449+PythonManRemastered@users.noreply.github.com>
2024-07-06 16:15:45 +0530

    Update slider.py

--- slider.py
+++ slider.py
@@ -32,7 +32,10 @@ project_intro = """
 
 header = st.container()
 header.title(f"How much have we spent: {money_spent}")
-header.write("""<div class='fixed-header'/>""", unsafe_allow_html=True)
+header.write("""
+<div style="position: fixed; top: 0; right: 0;">You have spent ${} </div>
+""", unsafe_allow_html=True)
+
 
 st.markdown(
     """
