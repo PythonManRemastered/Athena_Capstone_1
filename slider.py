4e58ebc7ca82e66556d6ba35a6eb79166cd17d52
PythonManRemastered <109542449+PythonManRemastered@users.noreply.github.com>
2024-07-12 22:59:40 +0530

    Update slider.py

--- slider.py
+++ slider.py
@@ -139,7 +139,7 @@ if st.session_state["begin_clicked"] and st.session_state["confirm_clicked"]:
             st.warning("Hey! You still haven't planted enough trees! Go back!")
         else:
             st.balloons()
-            st.write(f"Now that {st.session_state.tree_age} year(s) have passed, is there any difference in the amount of carbon sequestered?")     
+            st.write(f"Now that {tree_age} year(s) have passed, is there any difference in the amount of carbon sequestered?")     
             st.write(f"Last year, you chose to plant {st.session_state.tree_number} {selected_species} trees.")
             st.session_state.tree_age += 1
             tree_diameter += 1
