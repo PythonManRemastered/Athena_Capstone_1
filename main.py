979346af5a4a55f12ee0fce5a445e91186384916
PythonManRemastered <109542449+PythonManRemastered@users.noreply.github.com>
2024-09-24 20:53:12 +0530

    HTML Stack Integration

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ tree_height = 1
 tree_diameter = 1
 tree_age = 1
 cup_amount = 0 
-# height and diameter is in unit measurement
 
 # Initialize session state variables
 if "begin_clicked" not in st.session_state:
@@ -41,7 +40,6 @@ def update_cup_number(number):
     st.session_state.cup_amount = number
 
 # Main app UI
-
 st.header("What are we tackling in this cool article?")
 project_intro = """
 - Carbon credits are an incentive to decrease carbon emissions by allowing companies to buy 'credits' that allow for a certain amount of carbon emissions.
@@ -53,12 +51,12 @@ project_intro = """
 company_intro = """
 - You've been tasked with heading a medium-scale ceramic cup manufacturing facility's daily operations
 - You have been under a huge increase in popularity due to modern trends, and the cups that you make sell out almost immediately after they are listed!
-- You are looking through your paperwork and realise that you have exceeded the amount of carbon that you are allowed to emit
-- Everyday, you can only make a set number of cups, each of which need time to be fired and transported to sales
+- You are looking through your paperwork and realize that you have exceeded the amount of carbon that you are allowed to emit
+- Every day, you can only make a set number of cups, each of which need time to be fired and transported to sales
 - According to the manufacturing documents, you emitted approximately 2 kilograms of CO2 per cup, and about 8 kilograms of CO2 through the entire product development life cycle
 """
+
 first_year_prices_constant = 365
-# apple trees are placeholders
 first_year_prices = """
     Cost breakdown for year 1
 - Cost of each tree: $25
@@ -75,12 +73,10 @@ first_year_prices = """
 
 country_overviews_dictionary = {
     "South Africa": "The South African Provincial area is known for heavy acts of deforestation. Though this makes our job a lot harder, we can still try!",
-    "Zimbabwe": "Zimbabwe is a great place to start. According to the 17th Green Tax Amendment of Harare, deforestration is illegal in nearly all cases! This makes our job a lot easier",
-    "Zambia": "Zambia was one of the top regions responsible for 58% of all tree cover loss between 2001 and 2023. Though this might mean forestration policies there may be harder to implement, the country needs more tree cover. Perfect: Let's begin!",
-    "Russia": "Due to the current conflict in the Russian peninsula, co-ordinating forestation politically may prove to be a problem. Doesn't matter: Let's try!"
-
+    "Zimbabwe": "Zimbabwe is a great place to start. According to the 17th Green Tax Amendment of Harare, deforestation is illegal in nearly all cases! This makes our job a lot easier",
+    "Zambia": "Zambia was one of the top regions responsible for 58% of all tree cover loss between 2001 and 2023. Though this might mean forestation policies there may be harder to implement, the country needs more tree cover. Perfect: Let's begin!",
+    "Russia": "Due to the current conflict in the Russian peninsula, coordinating forestation politically may prove to be a problem. Doesn't matter: Let's try!"
 }
-
 
 # Display header with updated money spent
 def display_header_spent():
@@ -92,8 +88,6 @@ def display_header_spent():
     """, unsafe_allow_html=True)
 
 display_header_spent()
-
-
 
 def reset_everything():
     restarter_text = "Restarting article..."
@@ -109,9 +103,9 @@ def reset_everything():
     tree_height = 1
     tree_diameter = 1
     update_money_spent(0)
+
 if st.button("Click me if you want to start over"):
     reset_everything()
-    
 
 if st.button("Spend 100 buckaroos"):
     update_money_spent(100)
@@ -119,20 +113,11 @@ if st.button("Spend 100 buckaroos"):
 st.markdown(project_intro)
 st.caption("Note: Remember, you can hover over any of the terms in this article to see what they mean and where they've come from")
 
-
-
-
 # THIS IS THE BEGINNING OF THE CODE LOGIC #####################################################
-
-
-
-
-
 
 # Start button to begin the application
 if st.button("Click me to begin"):
     st.session_state["begin_clicked"] = True
-
 
 # Country selection and confirmation
 if st.session_state["begin_clicked"]:
@@ -143,12 +128,12 @@ if st.session_state["begin_clicked"]:
     st.markdown(f"Great! We have made {cups_amount} cups")
     global cups_price 
     cups_price = st.slider("Choose what these cups are priced at to the public: ", 0, 100)
-    st.markdown(f"Cool! Since our product is {cups_price} dollars per cup, we've made about {cups_price*cups_amount} dollars")
+    st.markdown(f"Cool! Since our product is {cups_price} dollars per cup, we've made about {cups_price * cups_amount} dollars")
     global money_made
-    money_made = cups_amount*cups_price
+    money_made = cups_amount * cups_price
     global carbon_emitted 
-    carbon_emitted = cups_amount*8
-    st.markdown(f"Unfortunately, this also means we have emitted about {8*cups_amount}kg of carbon! So, we now have to sequester this to become a green company")
+    carbon_emitted = cups_amount * 8
+    st.markdown(f"Unfortunately, this also means we have emitted about {8 * cups_amount}kg of carbon! So, we now have to sequester this to become a green company")
     
     selected_country = st.selectbox("Where do you want to establish the company?", countries)
     if st.button("Click me to confirm your choice!"):
@@ -157,118 +142,71 @@ if st.session_state["begin_clicked"]:
         country_overviews = country_overviews_dictionary.get(selected_country, 10)
         st.write(country_overviews)
 
-
-
 # Species selection and tree planting
 if st.session_state["begin_clicked"] and st.session_state["confirm_clicked"]:
     selected_species = st.selectbox("Choose the species of tree you want to plant:", species)
     
     tree_weights = {
-        "Red Maple": (tree_age * 0.75)/4.5,
-        "Silver Maple": (tree_age * 0.75)/3.0,
-        "Sugar Maple": (tree_age * 0.75)/5.0,
-        "River Birch": (tree_age * 0.75)/3.5,
-        "White Birch": (tree_age * 0.75)/5.0,
-        "Shagbark Hickory": (tree_age * 0.75)/7.5,
-        "Green Ash": (tree_age * 0.75)/4.0,
-        "Black Walnut": (tree_age * 0.75)/4.5,
-        "Black Cherry": (tree_age * 0.75)/5.0,
-        "White Oak": (tree_age * 0.75)/5.0,
-        "Red Oak": (tree_age * 0.75)/4.0,
-        "Pin Oak": (tree_age * 0.75)/3.0,
-        "Linden or Basswood": (tree_age * 0.75)/3.0,
-        "American Elm": (tree_age * 0.75)/4.0,
-        "Ironwood": (tree_age * 0.75)/7.0,
-        "Cottonwood": (tree_age * 0.75)/2.0,
-        "Dogwood": (tree_age * 0.75)/7.0,
-        "Redbud": (tree_age * 0.75)/7.0           
+        "Red Maple": (tree_age * 0.75) / 4.5,
+        "Silver Maple": (tree_age * 0.75) / 3.0,
+        "Sugar Maple": (tree_age * 0.75) / 5.0,
+        "River Birch": (tree_age * 0.75) / 3.5,
+        "White Birch": (tree_age * 0.75) / 5.0,
+        "Shagbark Hickory": (tree_age * 0.75) / 7.5,
+        "Green Ash": (tree_age * 0.75) / 4.0,
+        "Black Walnut": (tree_age * 0.75) / 4.5,
+        "Black Cherry": (tree_age * 0.75) / 5.0,
+        "White Oak": (tree_age * 0.75) / 5.0,
+        "Red Oak": (tree_age * 0.75) / 4.0,
+        "Pin Oak": (tree_age * 0.75) / 5.5,
+        "Linden or Basswood": (tree_age * 0.75) / 6.0,
+        "American Elm": (tree_age * 0.75) / 5.5,
+        "Ironwood": (tree_age * 0.75) / 6.5,
+        "Cottonwood": (tree_age * 0.75) / 4.0,
+        "Dogwood": (tree_age * 0.75) / 3.5,
+        "Redbud": (tree_age * 0.75) / 3.0
     }
-    # .get prevents key issues, 30 is the default value for non-recognizable species
-    tree_diameter = tree_weights.get(selected_species, 30)
     
-    tree_number = st.slider("Choose how many trees you want to plant", 0, 2000000)
-    update_tree_number(tree_number)
+    if st.button("Click me to plant a tree!"):
+        st.write(f"Awesome! You have planted a {selected_species} tree.")
+        carbon_offset = tree_weights[selected_species] * 1000 # kg of CO2 offset
+        st.write(f"This {selected_species} tree will sequester {carbon_offset:.2f} kg of CO2 over its lifetime.")
+        update_tree_number(1)
 
-    
-    st.write(f"Since you chose {selected_species}, each tree has a diameter, at breast height, of approximately {tree_diameter}")
-    st.write(f"You chose to plant {tree_number} {selected_species} trees.")
-    
-    carbon_sequestered_original = ((((((0.25*((tree_diameter)*(tree_diameter))*tree_height)*1.2)*0.725)*0.5)*3.67)* st.session_state.tree_number)/tree_age
-    st.write(f"Carbon sequestered in the first year: {carbon_sequestered_original} kgs")
-    
-    if carbon_sequestered_original > carbon_emitted:
-        st.subheader(f"Great! You've completely sequestered your carbon emissions with {tree_number} trees.")
-    else:
-        st.write(f"Not yet! We still need to sequester {carbon_emitted - int(carbon_sequestered_original)} kgs more!")
+if st.session_state["year_pass_clicked"]:
+    if st.button("Advance to the next year"):
+        update_tree_number(1)
+        st.write(f"After one year, you have successfully grown {st.session_state.tree_number} trees. These trees can now begin offsetting carbon!")
 
-    # Simulate time passing  
-    if st.button("Pass a Year"):
-        st.session_state["year_pass_clicked"] = True
-        progress_text = "Simulating one entire year passing..."
-        my_bar = st.progress(0, text=progress_text)
+# Footer with additional content and scrolling animation
+st.markdown("""
+<div style="position: relative; margin: 20px; background: #a51f1f;">
+    <div class="spacer" style="height: 100vh;"></div>
+    <div class="fade" style="opacity: 0; transition: opacity 1s ease-in-out; margin: 50px; padding: 20px; background: #fff; border: 1px solid #ccc;">Content 1</div>
+    <div class="spacer" style="height: 100vh;"></div>
+    <div class="fade" style="opacity: 0; transition: opacity 1s ease-in-out; margin: 50px; padding: 20px; background: #fff; border: 1px solid #ccc;">Content 2</div>
+    <div class="spacer" style="height: 100vh;"></div>
+    <div class="fade" style="opacity: 0; transition: opacity 1s ease-in-out; margin: 50px; padding: 20px; background: #fff; border: 1px solid #ccc;">Content 3</div>
+    <div class="spacer" style="height: 100vh;"></div>
+    <div class="fade" style="opacity: 0; transition: opacity 1s ease-in-out; margin: 50px; padding: 20px; background: #fff; border: 1px solid #ccc;">Content 4</div>
+    <div class="spacer" style="height: 100vh;"></div>
+</div>
 
-        for percent_complete in range(100):
-            time.sleep(0.01)
-            my_bar.progress(percent_complete + 1, text=progress_text)
-        time.sleep(1)
-        my_bar.empty() 
-        if carbon_sequestered_original < 1000:
-            st.warning("Hey! You still haven't planted enough trees! Go back!")
-        else:
-            st.balloons()
-            tree_age += 1
-            st.write(f"Now that {tree_age-1} year(s) have passed, is there any difference in the amount of carbon sequestered?")     
-            st.write(f"Last year, you chose to plant {st.session_state.tree_number} {selected_species} trees.")
-            tree_diameter += tree_age*(tree_diameter*(1/(tree_age*0.75)))
-            tree_height += 1
-            st.write("The tree diameter is ", tree_diameter)
-            st.write("The tree height is ", tree_height)
-            st.write("The tree age is ",tree_age)
-            
-            carbon_sequestered_original += (tree_age * st.session_state.tree_number * 20) / 100
-            
-            st.write(f"Carbon sequestered in the second year: {carbon_sequestered_original} kgs")
-        
-            st.write("Notice how the number didn't just double over the last year?")
-            expander = st.expander("Click me to see why")
-            expander.write("""
-                        As a tree grows up, it grows more wood to store carbon in. Hence, the amount of carbon a tree can sequester per year increases as it grows up.\n 
-                        Since the tree grows quite a bit during its first year of life, it becomes an exponentially larger carbon sink until the age of 5, after which\n
-                        there is a steady increase in a tree's ability to hold carbon
-            """)
-if st.session_state["begin_clicked"] and st.session_state["confirm_clicked"] and st.session_state["year_pass_clicked"] and carbon_sequestered_original > 1000:
-    st.error(f"""
-             Suddenly, the government of {selected_country} decides that the land you're using to plant these trees have oil under them. They send you a message to tell 
-             them how many trees they will have to cut down to clear the area so that they can figure out how much labour they'd need
-             """)
-    tree_number_lost = st.slider("Choose how many trees the government must cut down", 0, st.session_state.tree_number)
-    carbon_after_deforestation = (((((0.25*(tree_diameter*tree_diameter)*tree_height)*1.2)*0.725)*0.5)*3.67)* (st.session_state.tree_number - tree_number_lost)
-    carbon_lost = carbon_sequestered_original - carbon_after_deforestation
-    if st.button("Click me to confirm your choice"):
-        st.session_state["tree_cut_confirm"] = True
-        st.write(f"We lost {tree_number_lost} trees")
-        st.write(f"Because of this, about {carbon_lost} kgs of the carbon we sequestered is now back into the atmosphere")
-        
-if st.button("Click me to see how much this entire process costs!"):
-    st.session_state["carbon_offset_cost_confirm"] = True
-    
-if st.session_state["carbon_offset_cost_confirm"] and st.session_state["begin_clicked"] and st.session_state["confirm_clicked"] and st.session_state["year_pass_clicked"] and carbon_sequestered_original > 1000:
-    # tree sapling (current placeholder is 'apple trees') per sapling costs about 80 rupees, and may decrease per unit cost in larger scale forestation initiatives
-    st.markdown(f"Since we bought {tree_number} trees, and each sapling costs about 25 USD, we spent about {tree_number*25} USD on just buying the trees to plant them")
-    st.markdown("We also spent some money caring for the trees, and following is the cost breakdown for each tree. Though some of these costs are shared amongst trees, for the sake of simplicity, lets assume each tree requires these costs individually")
-    st.markdown(first_year_prices)
-    st.markdown(f"But remember, we made a lot of money from our cups. To be specific, we made about {money_made} USD")
-    st.markdown("Thus, our costs for this year can be calculated as the following:")
-    st.latex("Cost_Per_Tree * Number_of_trees - Sales * Price_of_each_cup")
-    st.markdown(f"This means that our total costs for our first year were about {(tree_number*first_year_prices_constant) - money_made} USD")
+<script>
+    document.addEventListener('scroll', () => {
+      const fadeElements = document.querySelectorAll('.fade');
+      const viewportHeight = window.innerHeight;
 
-if st.button("Click me to delve deeper!") and st.session_state["tree_cut_confirm"] and st.session_state["begin_clicked"] and st.session_state["confirm_clicked"] and st.session_state["year_pass_clicked"] and carbon_sequestered_original > 1000:
-        st.session_state["chapter_two_advance"] = True
-        st.header("So that's it....right? Did we just solve the global carbon sequestration?")
-        st.header("Like we have seen before, there are always more variables to take into consideration. So let's look further!")
-    
-# next chapter advancement button 
+      fadeElements.forEach(el => {
+        const elementTop = el.getBoundingClientRect().top;
 
-if st.session_state["begin_clicked"] and st.session_state["confirm_clicked"] and st.session_state["year_pass_clicked"] and st.session_state["chapter_two_advance"] and carbon_sequestered_original > 1000:
-    st.header("Chapter 2: What's next?")
-    st.subheader("insert additional c2 data)")
+        if (elementTop < viewportHeight) {
+          el.style.opacity = 1;
+        } else {
+          el.style.opacity = 0;
+        }
+      });
+    });
+</script>
+""", unsafe_allow_html=True)
+
